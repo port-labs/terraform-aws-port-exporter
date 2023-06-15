3e1f1730a7e322a8089d54731d1cedb2170db4fd
Matar Peles <matarpeles@Matars-MacBook-Pro.local>
2023-06-15 16:52:07 +0300

    updated the example

--- examples/run_module_example/main.tf
+++ examples/run_module_example/main.tf
@@ -1,10 +1,9 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 module "port_aws_exporter" {
   source = "git::https://github.com/port-labs/terraform-aws-port-exporter.git"
-  
-  stack_name                       = var.stack_name
-  secret_name                      = var.secret_name
-  create_bucket                    = var.create_bucket
-  bucket_name                      = var.bucket_name
-  config_json_file                 = var.config_json
-  function_name                    = var.function_name
+  config_json   = "config.json"
+  lambda_policy = "../defaults/policy.json"
+  bucket_name = "port-aws-exporter-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
 }
